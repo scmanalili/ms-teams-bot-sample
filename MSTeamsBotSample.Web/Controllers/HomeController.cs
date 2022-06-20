526b99e88c541266bbd28438d72178e1cec90fbd
Sean Manalili <Seanm@pageuppeople.com>
2022-06-20 12:35:08 +0800

    initial commit.

---
+++ MSTeamsBotSample.Web/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MSTeamsBotSample.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("hello")]
+        public ActionResult Hello()
+        {
+            return View("Index");
+        }
+
+        [Route("first")]
+        public ActionResult First()
+        {
+            return View();
+        }
+
+        [Route("second")]
+        public ActionResult Second()
+        {
+            return View();
+        }
+
+        [Route("configure")]
+        public ActionResult Configure()
+        {
+            return View();
+        }
+    }
+}
---
+++ MSTeamsBotSample.Web/Startup.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MSTeamsBotSample.Web.Bots;
+
+namespace MSTeamsBotSample.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddMvc();
+
+            services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+            services.AddTransient<IBot, NotificationBot>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseWebSockets();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                   name: "default",
+                   pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            });
+        }
+    }
+}
